731b606a4da887d7fe402ce1ea26888a78959e84
macmaku <dziadykkm@gmail.com>
2017-12-09 00:08:56 +0100

    clear code

---
+++ src/sprites/Player.js
@@ -0,0 +1,82 @@
+import Phaser from 'phaser'
+
+export default class extends Phaser.Sprite {
+    constructor({game, x, y, asset}) {
+        super(game, x, y, asset);
+        this
+            .anchor
+            .setTo(0.5);
+        this
+            .game
+            .physics
+            .enable(this, Phaser.Physics.ARCADE);
+
+        this.timeToStep = this.game.time.now;
+        this
+            .animations
+            .add('left', [
+                0, 1, 2, 3
+            ], 10, true);
+        this
+            .animations
+            .add('turn', [4], 20, true);
+        this
+            .animations
+            .add('right', [
+                5, 6, 7, 8
+            ], 10, true);
+        this.body.velocity.x = 0;
+        this.facing = 'idle';
+        this.jumpTimer = this.game.time.now;
+
+    }
+
+    moveLeft() {
+
+        this.body.velocity.x = -150;
+
+        if (this.facing != 'left') {
+            this
+                .animations
+                .play('left');
+            this.facing = 'left';
+        }
+    }
+    moveRight() {
+
+        this.body.velocity.x = 150;
+
+        if (this.facing != 'right') {
+            this
+                .animations
+                .play('right');
+            this.facing = 'right';
+        }
+    }
+
+    stop() {
+        this
+            .animations
+            .stop();
+
+        if (this.facing == 'left') {
+            this.frame = 0;
+        } else {
+            this.frame = 5;
+        }
+
+        this.facing = 'idle';
+    }
+
+
+    jump(){
+
+        if(this.body.onFloor()){
+            this.body.velocity.y = -250;
+            this.jumpTimer = this.game.time.now;
+        }
+
+    }
+
+    update() {}
+}
--- src/states/Game.js
+++ src/states/Game.js
@@ -1,13 +1,12 @@
 import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
 import Enemy from '../sprites/Enemy'
+import Player from '../sprites/Player'
 
 export default class extends Phaser.State {
 
   constructor() {
     super()
-    this.facing = 'left';
-    this.jumpTimer = 0;
   }
 
   init() { }
@@ -29,11 +28,14 @@ export default class extends Phaser.State {
     this.game.physics.arcade.gravity.y = 520;
 
     //PLAYER
-    this.player = game.add.sprite(32, 32, 'dude');
-    this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
-    this.player.body.bounce.y = 0.2;
-    this.player.body.collideWorldBounds = true;
-    this.player.body.setSize(20, 32, 5, 16);
+    this.player = new Player({
+      game: this.game,
+      x: this.world.centerX,
+      y: this.world.centerY,
+      asset: 'dude'
+    })
+
+    this.game.add.existing(this.player);
 
     //ENEMY
     this.enemy = new Enemy({
@@ -44,10 +46,7 @@ export default class extends Phaser.State {
     })
     this.game.add.existing(this.enemy);
 
-    //PLAYER ANIMATIONS
-    this.player.animations.add('left', [0, 1, 2, 3], 10, true);
-    this.player.animations.add('turn', [4], 20, true);
-    this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+
 
     //GAME CAMERA, CURSORS
     this.game.camera.follow(this.player);
@@ -62,46 +61,30 @@ export default class extends Phaser.State {
     
 
     this.player.body.velocity.x = 0;
-
-    if (this.cursors.left.isDown) {
-      this.player.body.velocity.x = -150;
-
-      if (this.facing != 'left') {
-        this.player.animations.play('left');
-        this.facing = 'left';
-      }
-    }
-    else if (this.cursors.right.isDown) {
-      this.player.body.velocity.x = 150;
-
-      if (this.facing != 'right') {
-        this.player.animations.play('right');
-        this.facing = 'right';
-      }
-    }
-    else {
-      if (this.facing != 'idle') {
-        this.player.animations.stop();
-
-        if (this.facing == 'left') {
-          this.player.frame = 0;
-        }
-        else {
-          this.player.frame = 5;
-        }
-
-        this.facing = 'idle';
-      }
-    }
-
-    if (this.jumpButton.isDown && this.player.body.onFloor() && this.game.time.now > this.jumpTimer) {
-      this.player.body.velocity.y = -250;
-      this.jumpTimer = this.game.time.now + 750;
-    }
+    this.movementPlayer();
 
   }
 
   render() {
 
+  }
+
+
+
+  movementPlayer(){
+    if (this.cursors.left.isDown) {
+      this.player.moveLeft();
+    }
+    else if (this.cursors.right.isDown) {
+      this.player.moveRight();
+    }
+    else if (this.facing != 'idle') {
+       this.player.stop();
+    }
+    
+
+    if (this.jumpButton.isDown ) {
+      this.player.jump();
+    }
   }
 }
