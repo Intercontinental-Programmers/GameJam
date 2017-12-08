ac03c7a30929067882e8032bb0e0b3d120aa2c6f
Maciej Hajduk <ebrithil97@gmail.com>
2017-12-08 22:59:29 +0100

    map and sprites

--- src/states/Game.js
+++ src/states/Game.js
@@ -3,32 +3,94 @@ import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
 
 export default class extends Phaser.State {
-  init () {}
-  preload () {}
 
-  create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
-
-    this.game.add.existing(this.mushroom)
+  constructor() {
+    super()
+    this.facing = 'left';
+    this.jumpTimer = 0;
   }
 
-  render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+  init() { }
+  preload() { }
+
+  create() {
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //LOADING BACKGROUND
+    this.bg = game.add.tileSprite(0, 0, 900, 600, 'background');
+    this.bg.fixedToCamera = true;
+
+    //LOADING MAP
+    this.map = game.add.tilemap('level1');
+    this.map.addTilesetImage('tiles-1');
+    this.map.setCollisionByExclusion([13, 14, 15, 16, 46, 47, 48, 49, 50, 51]);
+    this.layer = this.map.createLayer('Tile Layer 1');
+    this.layer.resizeWorld();
+    this.game.physics.arcade.gravity.y = 520;
+
+    //PLAYER
+    this.player = game.add.sprite(32, 32, 'dude');
+    this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+    this.player.body.bounce.y = 0.2;
+    this.player.body.collideWorldBounds = true;
+    this.player.body.setSize(20, 32, 5, 16);
+
+    //PLAYER ANIMATIONS
+    this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.player.animations.add('turn', [4], 20, true);
+    this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    //GAME CAMERA, CURSORS
+    this.game.camera.follow(this.player);
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+    this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  }
+
+  update() {
+
+    this.game.physics.arcade.collide(this.player, this.layer);
+
+    this.player.body.velocity.x = 0;
+
+    if (this.cursors.left.isDown) {
+      this.player.body.velocity.x = -150;
+
+      if (this.facing != 'left') {
+        this.player.animations.play('left');
+        this.facing = 'left';
+      }
     }
+    else if (this.cursors.right.isDown) {
+      this.player.body.velocity.x = 150;
+
+      if (this.facing != 'right') {
+        this.player.animations.play('right');
+        this.facing = 'right';
+      }
+    }
+    else {
+      if (this.facing != 'idle') {
+        this.player.animations.stop();
+
+        if (this.facing == 'left') {
+          this.player.frame = 0;
+        }
+        else {
+          this.player.frame = 5;
+        }
+
+        this.facing = 'idle';
+      }
+    }
+
+    if (this.jumpButton.isDown && this.player.body.onFloor() && this.game.time.now > this.jumpTimer) {
+      this.player.body.velocity.y = -250;
+      this.jumpTimer = this.game.time.now + 750;
+    }
+
+  }
+
+  render() {
+
   }
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -13,7 +13,14 @@ export default class extends Phaser.State {
     //
     // load your assets
     //
-    this.load.image('mushroom', 'assets/images/mushroom2.png')
+    this.load.image('mushroom', 'assets/images/mushroom2.png'),
+    this.load.image('tiles-1', 'assets/tiles-1.png'),
+    this.load.spritesheet('dude', 'assets/dude.png', 37, 50),
+    this.load.spritesheet('droid', 'assets/droid.png', 32, 32),
+    this.load.image('starSmall', 'assets/star.png'),
+    this.load.image('starBig', 'assets/star2.png'),
+    this.load.image('background', 'assets/background2.png')
+    this.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
   }
 
   create () {
