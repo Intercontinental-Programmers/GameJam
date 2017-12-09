5e8d4ee4b641a1896d8bd51c66f907bbb8548476
macmaku <dziadykkm@gmail.com>
2017-12-09 02:25:52 +0100

    add_fov

--- src/sprites/Enemy.js
+++ src/sprites/Enemy.js
@@ -1,14 +1,23 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
-  constructor ({ game, x, y, asset }) {
+  constructor({game, x, y, asset}) {
     super(game, x, y, asset),
-    this.anchor.setTo(0.5),
-    this.game.physics.arcade.enable(this),
+    this
+      .anchor
+      .setTo(0.5),
+    this
+      .game
+      .physics
+      .arcade
+      .enable(this),
     //this.enableBody = true,
     this.timeToStep = this.game.time.now;
 
-    //speed of movement 
+    //this init graphic for polygon
+    this.graphics = this.game.add.graphics(0, 0);
+
+    //speed of movement
     this.speed = 70;
     //modes
     this.attackMode = 0;
@@ -18,33 +27,101 @@ export default class extends Phaser.Sprite {
     // direction to move
     this.moveLeft = 1;
     this.moveRight = 0;
+    this.polyOfViewRight = new Phaser.Polygon([
+      new Phaser.Point(this.x, this.y - 23),
+      new Phaser.Point(this.x + 100, this.y - 48),
+      new Phaser.Point(this.x + 100, this.y + 25)
+    ]);
+    this.polyOfViewLeft = new Phaser.Polygon([
+      new Phaser.Point(this.x, this.y - 25),
+      new Phaser.Point(this.x - 100, this.y - 48),
+      new Phaser.Point(this.x - 100, this.y + 25)
+    ]);
+
+    this.polyOfViewRight2 = new Phaser.Polygon([
+      new Phaser.Point(this.x-9, this.y - 10),
+      new Phaser.Point(this.x + 66.7, this.y - 24.35),
+      new Phaser.Point(this.x + 66.7, this.y + 24.35)
+    ]);
+
+    this.polyOfViewLeft2 = new Phaser.Polygon([
+      new Phaser.Point(this.x-9, this.y - 10),
+      new Phaser.Point(this.x - 66.7, this.y - 24.35),
+      new Phaser.Point(this.x - 66.7, this.y + 24.35)
+    ]);
+
   }
 
-  wander () {
-    if(this.moveLeft === 1){
-      console.log("Ruszam sie w lewo");
+  wander() {
+    if (this.moveLeft === 1) {
       this.body.velocity.x = -this.speed;
       this.moveLeft = 0;
       this.moveRight = 1;
-    }
-    else if(this.moveRight === 1){
-      console.log("Ruszam sie w prawo");
+    } else if (this.moveRight === 1) {
       this.body.velocity.x = this.speed;
       this.moveRight = 0;
       this.moveLeft = 1;
     }
   }
 
-  update () {
+  drawView() {
+    
+    
+
+    this.graphics.beginFill(0xFF33ff);
+    if(this.moveLeft === 0){
+      this.graphics.drawPolygon(this.polyOfViewLeft.points);
+      this.graphics.beginFill(0xFFFFff);
+      this.graphics.drawPolygon(this.polyOfViewLeft2.points);
+    }
+    else{
+      this.graphics.beginFill(0xFF33ff);
+      this.graphics.drawPolygon(this.polyOfViewRight.points);
+      this.graphics.beginFill(0xFFFFff);
+      this.graphics.drawPolygon(this.polyOfViewRight2.points);
+    }
+
+    this.graphics.endFill();
+  }
+
+  update() {
     //delta of time
     this.diff_time = this.game.time.now - this.timeToStep;
     //console.log(this.diff_time);
-    if(this.wanderMode === 1 && this.diff_time > 2000){
+    if (this.wanderMode === 1 && this.diff_time > 2000) {
       //this.body.velocity.setTo(0, 0);
       this.timeToStep = this.game.time.now;
-      console.log('poruszam sie w ' + this.moveLeft + "L" + "   " + this.moveRight + "R");
       this.wander();
     }
-      
+
+
+    this.polyOfViewRight = new Phaser.Polygon([
+      new Phaser.Point(this.x-9, this.y - 10),
+      new Phaser.Point(this.x + 100, this.y - 47),
+      new Phaser.Point(this.x + 100, this.y + 25)
+    ]);
+
+    this.polyOfViewRight2 = new Phaser.Polygon([
+      new Phaser.Point(this.x-9, this.y - 10),
+      new Phaser.Point(this.x + 66.7, this.y - 36.35),
+      new Phaser.Point(this.x + 66.7, this.y + 14.95)
+    ]);
+
+    this.polyOfViewLeft2 = new Phaser.Polygon([
+      new Phaser.Point(this.x-9, this.y - 10),
+      new Phaser.Point(this.x - 80.7, this.y - 34.35),
+      new Phaser.Point(this.x - 80.7, this.y + 13.30)
+    ]);
+
+    this.polyOfViewLeft = new Phaser.Polygon([
+      new Phaser.Point(this.x-9, this.y - 10),
+      new Phaser.Point(this.x - 120, this.y - 47),
+      new Phaser.Point(this.x - 120, this.y + 26)
+    ]);
+    this.graphics.clear();
+    this.drawView();
+
+
+
   }
 }
