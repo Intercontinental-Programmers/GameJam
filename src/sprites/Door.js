d0526e71931824d7a7a1297c8349d53670e32ad2
Maciej Hajduk <ebrithil97@gmail.com>
2017-12-09 16:46:22 +0100

    multiple layers

--- src/sprites/Door.js
+++ src/sprites/Door.js
@@ -21,8 +21,6 @@ export default class extends Phaser.Sprite {
     //kills particular door -> deletes collision
     //needed review from John
     unlockDoor(player, door) {
-        console.log(player.inventory);
-        console.log(door.key);
         for (var counter in player.inventory) {
             if (player.inventory[counter] == door.key)
                 door.kill();
--- src/states/Game.js
+++ src/states/Game.js
@@ -23,12 +23,17 @@ export default class extends Phaser.State {
     this.bg.fixedToCamera = true;
 
     //LOADING MAP
-    this.map = game.add.tilemap('level1');
-    this.map.addTilesetImage('tiles-1');
-    this.map.setCollisionByExclusion([13, 14, 15, 16, 46, 47, 48, 49, 50, 51]);
-    this.layer = this.map.createLayer('Tile Layer 1');
-    this.layer.resizeWorld();
     this.game.physics.arcade.gravity.y = 800;
+    this.tilemap = this.game.add.tilemap('level');
+    this.tilemap.addTilesetImage('tiles-1');
+    this.tilemap.setCollisionByExclusion([13, 14, 15, 16, 46, 47, 48, 49, 50, 51]);
+    this.tilemap.myLayers = [];
+    for (var i = 2; i > 0; i--) {
+      this.layer = this.tilemap.createLayer('layer' + i);
+      this.layer.resizeWorld();
+      this.tilemap.myLayers[i - 1] = this.layer;
+    }
+
 
     //PLAYER
     this.player = new Player({
@@ -37,9 +42,6 @@ export default class extends Phaser.State {
       y: 50,
       asset: 'dude'
     })
-
-    
-    
     this.game.add.existing(this.player);
 
     //ENEMIES
@@ -87,7 +89,7 @@ export default class extends Phaser.State {
     this.killButtonFlag = true;
   }
 
-  addNewEnemy(posX, posY){
+  addNewEnemy(posX, posY) {
 
     this.enemies.add(new Enemy({
       game: this.game,
@@ -99,8 +101,8 @@ export default class extends Phaser.State {
     console.log('enemy created');
   }
 
-  genRandomSpawnPoint(){
-      return this.game.rnd.integerInRange(16, this.game.width - 16);
+  genRandomSpawnPoint() {
+    return this.game.rnd.integerInRange(16, this.game.width - 16);
   }
 
   update() {
@@ -108,7 +110,6 @@ export default class extends Phaser.State {
     this.game.physics.arcade.collide(this.player, this.layer);
     this.game.physics.arcade.collide(this.enemies, this.layer);
     this.game.physics.arcade.collide(this.player, this.enemies, this.simpleCollision);
-    //this.game.physics.arcade.collide(this.enemy, this.layer);
     this.game.physics.arcade.collide(this.door, this.layer);
     this.game.physics.arcade.collide(this.key, this.layer);
     this.game.physics.arcade.collide(this.player, this.door, this.door.unlockDoor);
@@ -116,10 +117,10 @@ export default class extends Phaser.State {
 
     this.enemies.setAll('body.immovable', true);
     // this.enemy.body.immovable = true;
-  
+
     this.player.body.velocity.x = 0;
     this.movementPlayer();
-    
+
   }
 
   key_collector(player, key) {
@@ -151,57 +152,57 @@ export default class extends Phaser.State {
       this.player.jump();
     }
 
-    if(this.killButton.isDown){
-      
-      if(this.killButtonFlag){
-        
+    if (this.killButton.isDown) {
+
+      if (this.killButtonFlag) {
+
 
         this.enemies.forEach(enemy => {
           //probably requires refactor
-          if(Math.abs(this.player.x - enemy.x) < 50){
-            
-            if(this.lookingAtEnemy(this.player, enemy)){
-               console.log('zabiles typa');
-               enemy.killEnemy();
-               this.enemies.remove(enemy);
+          if (Math.abs(this.player.x - enemy.x) < 50) {
+
+            if (this.lookingAtEnemy(this.player, enemy)) {
+              console.log('zabiles typa');
+              enemy.killEnemy();
+              this.enemies.remove(enemy);
             }
-            else{
-               console.log('nie zabiles typa');
+            else {
+              console.log('nie zabiles typa');
             }
           }
-          else{
-              console.log('nie zabiles typa')
+          else {
+            console.log('nie zabiles typa')
           }
         })
-       
+
         this.killButtonFlag = false;
       }
     }
 
-    if(this.killButton.isUp){
+    if (this.killButton.isUp) {
       this.killButtonFlag = true;
     }
   }
 
   //probably requires refactor
-  lookingAtEnemy(player, enemy){
-  
-    if(player.facing == 'left'){
+  lookingAtEnemy(player, enemy) {
+
+    if (player.facing == 'left') {
       return (player.body.x > enemy.body.x) && this.areOnTheSameLevel(player, enemy);
     }
-    else if(player.facing == 'right'){
+    else if (player.facing == 'right') {
       return (player.body.x < enemy.body.x) && this.areOnTheSameLevel(player, enemy);
     }
-    else{
+    else {
       return this.areOnTheSameLevel(player, enemy);
     }
   }
 
-  areOnTheSameLevel(player, enemy){
+  areOnTheSameLevel(player, enemy) {
     return (player.body.y == enemy.body.y)
   }
 
   simpleCollision(player, enemy) {
-      enemy.body.velocity.x = 0;
+    enemy.body.velocity.x = 0;
   }
 }
