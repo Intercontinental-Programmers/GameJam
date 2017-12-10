17a75176603bf8d689bdeaa78cf430d54346c16f
Maciej Hajduk <ebrithil97@gmail.com>
2017-12-10 07:03:00 +0100

    gameover screen

--- src/states/Credits.js
+++ src/states/Credits.js
@@ -3,6 +3,7 @@ import WebFont from 'webfontloader'
 
 export default class extends Phaser.State {
   init() {
+    this.stage.backgroundColor = '#000'
     this.fontsReady = false
     this.fontsLoaded = this.fontsLoaded.bind(this)
     this.buttonCliked = false;
@@ -18,10 +19,11 @@ export default class extends Phaser.State {
   }
 
   create() {
-    var image = game.add.image(this.world.centerX, this.world.centerY - 100, 'logo')
-    let text = this.add.text(this.world.centerX, this.world.centerY, 'Maciej Dziadyk\nJakubDudycz\nMaciej Hajduk\nJan Librowski\nMateusz Walczak', { font: '10px Arial', fill: '#000000', align: 'center' })
+    let text = this.add.text(this.game.width * 0.5, this.game.height * 0.3, 'Organization', { font: '100px Sheriff', fill: '#fff', align: 'center' })
     text.anchor.setTo(0.5, 0.5)
-    let button = game.add.button(game.world.centerX, this.world.centerY + 150, 'button', this.actionOnClick, this, 2, 1, 0);
+    let text1 = this.add.text(this.game.width * 0.5, this.game.height * 0.5, 'Maciej Dziadyk\nJakubDudycz\nMaciej Hajduk\nJan Librowski\nMateusz Walczak', { font: '10px Arial', fill: '#fff', align: 'center' })
+    text1.anchor.setTo(0.5, 0.5)
+    let button = game.add.button(this.game.width * 0.5, this.game.height * 0.8, 'button', this.actionOnClick, this, 2, 1, 0);
     button.anchor.setTo(0.5)
   }
 
--- src/states/Menu.js
+++ src/states/Menu.js
@@ -5,7 +5,7 @@ export default class extends Phaser.State{
 
     preload(){
     
-        let text = this.add.text(this.world.centerX, this.world.centerY - 120, 'Organization', { font: '100px Sheriff', fill: '#fff', align: 'center' })
+        let text = this.add.text(this.game.width * 0.5, this.game.height * 0.3, 'Organization', { font: '100px Sheriff', fill: '#fff', align: 'center' })
         text.anchor.setTo(0.5, 0.5)
         this.btnPlay = this.game.add.button(this.game.width * 0.5, this.game.height * 0.7, 'btnPlay', this.startGame, this, 2, 1, 0)
         this.btnPlay.anchor.setTo(0.5)
