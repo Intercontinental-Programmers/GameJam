314046c30b2c255ae76d84971859556a4b3487bd
Jakub Dudycz <dudyczjakub@gmail.com>
2017-12-10 12:17:49 +0100

    stage menu fixed

--- src/states/StageMenu.js
+++ src/states/StageMenu.js
@@ -11,8 +11,8 @@ export default class extends Phaser.State {
     game.load.spritesheet('mission3_button', 'assets/buttons/mission3.png', 153, 167);
     game.load.spritesheet('mission4_button', 'assets/buttons/mission4.png', 153, 167);
     game.load.spritesheet('mission5_button', 'assets/buttons/mission5.png', 153, 167);
-    game.load.spritesheet('mission1_locked_button', 'assets/buttons/mission1_locked.png', 153, 167);
-    game.load.spritesheet('mission2_locked_button', 'assets/buttons/mission2_locked.png', 153, 167);
+    game.load.spritesheet('mission1_locked_button', 'assets/buttons/mission1 _locked.png', 153, 167);
+    game.load.spritesheet('mission2_locked_button', 'assets/buttons/mission2 _locked.png', 153, 167);
     game.load.spritesheet('mission3_locked_button', 'assets/buttons/mission3_locked.png', 153, 167);
     game.load.spritesheet('mission4_locked_button', 'assets/buttons/mission4_locked.png', 153, 167);
     game.load.spritesheet('mission5_locked_button', 'assets/buttons/mission5_locked.png', 153, 167);
@@ -30,16 +30,16 @@ export default class extends Phaser.State {
 
     this.background = game.add.tileSprite(0, 0, 900, 600, 'background');
 
-    this.mission1_button = game.add.button(game.world.centerX - 100, 180, 'mission-completed_button', this.actionOnClick, this, 2, 1, 0);
-    this.mission2_button = game.add.button(game.world.centerX - 20, 250, 'mission2_button', this.startGame, this, 2, 1, 0);
+    this.mission1_button = game.add.button(game.world.centerX - 100, 180, 'mission1_button', this.startGame, this, 2, 1, 0);
+    this.mission2_button = game.add.button(game.world.centerX - 20, 250, 'mission2_locked_button',this.actionOnClick, this, 2, 1, 0);
     this.mission3_button = game.add.button(game.world.centerX - 120, 320, 'mission3_locked_button', this.actionOnClick, this, 2, 1, 0);
     this.mission4_button = game.add.button(game.world.centerX - 250, 350, 'mission4_locked_button', this.actionOnClick, this, 2, 1, 0);
     this.mission5_button = game.add.button(game.world.centerX - 400, 370, 'mission5_locked_button', this.actionOnClick, this, 2, 1, 0);
 
 
     this.missionButton_state = new Map();
-    this.missionButton_state.set(this.mission1_button, {state: "completed", image: 'mission-completed_button'});
-    this.missionButton_state.set(this.mission2_button, {state: "unlocked", image: 'mission2_button'});
+    this.missionButton_state.set(this.mission1_button, {state: "unlocked", image: 'mission1_button'});
+    this.missionButton_state.set(this.mission2_button, {state: "locked", image: 'mission2_locked_button'});
     this.missionButton_state.set(this.mission3_button, {state: "locked", image: 'mission3_locked_button'});
     this.missionButton_state.set(this.mission4_button, {state: "locked", image: 'mission4_locked_button'});
     this.missionButton_state.set(this.mission5_button, {state: "locked", image: 'mission5_locked_button'});
